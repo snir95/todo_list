dfdc16a00e2e02d5164231245f9a512435dc32cd
Snir <snir_60@Yahoo.com>
2022-05-29 15:20:57 +0300

    1
    1

--- NativeProject/components/ListItem.js
+++ NativeProject/components/ListItem.js
@@ -5,22 +5,30 @@ import {
   View,
   StyleSheet,
   TextInput,
+  Switch,
 } from 'react-native';
-// import {Icon} from 'react-native-vector-icons/dist/FontAwesome';
 import CustomButton from './buttons/CustomButton';
 import {useUpdateTaskMutation} from '../services/task';
 
 const ListItem = ({item, handlers}) => {
   const [editable, setEditable] = useState(false);
-  const [text, setText] = useState('');
+
+  // const [text, setText] = useState('');
+  // const [bColor, setBColor] = useState('white');
+  // const [isActivated, setIsActivated] = useState(true);
+  // const [tagged, setTagged] = useState('');
+
+  const [currTask, setCurrTask] = useState(item);
+  console.log(currTask, 'blablabla');
   const [updateTask, updateResult] = useUpdateTaskMutation();
-  const handleEdit = () => {
+  const handleEdit = currTask => {
+    console.log(currTask, 'edit shit');
     updateTask({
-      _id: item._id,
-      title: text,
-      bgColor: 'white',
-      isActive: true,
-      tag: 'home',
+      _id: currTask._id,
+      title: currTask.title,
+      bgColor: currTask.bgColor,
+      isActive: currTask.isActive,
+      tag: currTask.tag || 'home',
     });
   };
   return (
@@ -44,14 +52,34 @@ const ListItem = ({item, handlers}) => {
         <View style={styles.listItemView}>
           <TextInput
             style={styles.input}
-            onChangeText={text => setText(text)}
-            defaultValue={item.title}></TextInput>
+            onChangeText={title => setCurrTask({...currTask, title})}
+            defaultValue={item.title}
+          />
+
+          <TextInput
+            style={styles.input}
+            onChangeText={bgColor => setCurrTask({...currTask, bgColor})}
+            defaultValue={item.bgColor}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={tag => setCurrTask({...currTask, tag})}
+            defaultValue={item.tag}
+          />
+
+          <Switch
+            style={styles.input}
+            onValueChange={isActive => setCurrTask({...currTask, isActive})}
+            value={{isActive}}
+          />
+
           <CustomButton
             title={'save'}
             onPress={() => {
-              handleEdit(item), setEditable(false);
+              currTask && handleEdit(currTask);
+              setEditable(false);
             }}
-            defaultValue={text}
+            defaultValue={item}
           />
         </View>
       )}
@@ -62,7 +90,6 @@ const ListItem = ({item, handlers}) => {
 const styles = StyleSheet.create({
   listItem: {
     padding: 15,
-    backgroundColor: '#f8f8f8',
     borderBottomWidth: 1,
     borderColor: '#eee',
   },
